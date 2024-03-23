78b60dc7e25b7bf6b4d0cf9896b568fae21c216c
Ian Homer <code@ianhomer.com>
2024-03-23 06:51:54 +0000

    ⚗️ basic syntax examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
+<script>
+  import Component from "./component.svelte";
+  const foo = 1;
+
+  let count = 0;
+
+  $: doubled = count * 2;
+  $: quadrupled = doubled * 2;
+  $: if (count >= 10) {
+    alert(`count is high`);
+  }
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
 <h1>Try Svelte</h1>
-<p>This is it</p>
+<p>Value of foo {foo}; double foo {foo * 2}</p>
+<p class="foo">Styled content</p>
+<button on:click={handleClick}>
+  Clicked {count} ; {doubled} : {quadrupled}
+  {count === 1 ? "time" : "times"}
+</button>
+<Component />
+
+<style>
+  p.foo {
+    color: purple;
+  }
+</style>
